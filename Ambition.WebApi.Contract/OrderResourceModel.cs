3c25d345756e102afe3025d32ae9559bcb249567
Eugene Yalandaev <yalandaev@gmail.com>
2018-01-05 23:06:33 +0400

    Added angular applications

---
+++ Ambition.WebApi.Contract/OrderResourceModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ambition.WebApi.Contract
+{
+    public class OrderResourceModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public List<OrderLineResourceModel> OrderLines { get; set; }
+    }
+
+    public class OrderLineResourceModel
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Ambition.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ambition.WebApi.WebApiApplication" Language="C#" %>
