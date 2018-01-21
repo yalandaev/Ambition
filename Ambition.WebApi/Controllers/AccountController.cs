cec6f507edd1a4b35116d20782378ce2dc249b93
Eugene Yalandaev <yalandaev@gmail.com>
2018-01-21 17:28:41 +0400

    Bearer token auth

---
+++ Ambition.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Http;
+using Ambition.WebApi.Models;
+
+namespace Ambition.WebApi.Controllers
+{
+    [Authorize]
+    public class AccountController : ApiController
+    {
+        [HttpGet]
+        [Route("api/account/getuser/{id}")]
+        public IHttpActionResult GetUFirstUser(int id)
+        {
+            // Get user from dummy list
+            var userList = new List<User>()
+            {
+                new User() { Name = "admin", Email = "admin@app.com", Id = 1, Password = "secret"}
+            };
+            var user = userList.FirstOrDefault(x => x.Id == id);
+
+            var currentUser = HttpContext.Current.User;
+            var identity = (ClaimsIdentity)User.Identity;
+            IEnumerable<Claim> claims = identity.Claims;
+
+            return Ok(user);
+        }
+    }
+}
---
+++ Ambition.WebApi/Services/UserService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ambition.WebApi.Models;
+
+namespace Ambition.WebApi.Services
+{
+    public class UserService
+    {
+        public User ValidateUser(string email, string password)
+        {
+            // Here you can write the code to validate
+            // User from database and return accordingly
+            // To test we use dummy list here
+            var userList = GetUserList();
+            var user = userList.FirstOrDefault(x => x.Email == email && x.Password == password);
+            return user;
+        }
+
+        public List<User> GetUserList()
+        {
+            return new List<User>()
+            {
+                new User() { Name = "admin", Email = "admin@app.com", Id = 1, Password = "secret"}
+            };
+        }
+    }
+}
